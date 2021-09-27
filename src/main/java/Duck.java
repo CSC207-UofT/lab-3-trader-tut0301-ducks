67498e025530e3cede40d5026a5da164bc9d77ad
purplepaw <saynas010@gmail.com>
2021-09-27 15:47:34 -0400

    Added duck class!

---
+++ src/main/java/Duck.java
@@ -0,0 +1,13 @@
+/* A sample class that implements the given interfaces.
+ */
+public class Duck implements Domesticatable, Tradable {
+
+    public Duck() {}
+    @Override
+    public String sound() {
+        return "Quack!";
+    }
+
+    @Override
+    public int getPrice() {return 5;}
+}
