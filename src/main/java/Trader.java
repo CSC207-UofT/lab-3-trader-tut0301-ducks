4e6354f19945d8d2a894a5627367290308181628
KowanChan <kowan2002@outlook.com>
2021-09-27 14:50:30 -0400

    New Branch

--- src/main/java/Trader.java
+++ src/main/java/Trader.java
@@ -96,8 +96,13 @@ public class Trader {
         List<Tradable> all_items = Arrays.asList(
                 new Horse(),
                 new Horse(),
-                new Horse()
-                // TODO: Add Tradable objects here!
+                new Horse(),
+                new Tradable() {
+                    @Override
+                    public int getPrice() {
+                        return 10;
+                    }
+                }
         );
 
         /* Below, we've created two Traders. Their money, inventory, and
